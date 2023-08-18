1806837c933c7a10f2854ca39f7047d2e3c0bdd1
gamerfoxx <admin@josephludbrook.com>
2023-08-18 11:02:30 -0400

    add delete button for edit expense

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { useLayoutEffect } from 'react';
 
+import IconButton from '../components/UI/IconButton';
+import { GlobalStyles } from '../constants/styles';
+
 function ManageExpenseScreen({ route, navigation }) {
 	const editedExpenseId = route.params?.expenseId;
 	const isEdited = !!editedExpenseId;
@@ -11,7 +14,37 @@ function ManageExpenseScreen({ route, navigation }) {
 		});
 	}, [navigation, isEdited]);
 
-	return <Text>ManageExpenseScreen</Text>;
+	function deleteExpenseHandler() {}
+
+	return (
+		<View style={styles.container}>
+			{isEdited && (
+				<View style={styles.deleteContainer}>
+					<IconButton
+						icon="trash"
+						color={GlobalStyles.colors.error2}
+						size={35}
+						onPress={deleteExpenseHandler}
+					/>
+				</View>
+			)}
+		</View>
+	);
 }
 
 export default ManageExpenseScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 25,
+		backgroundColor: GlobalStyles.colors.primary7,
+	},
+	deleteContainer: {
+		marginTop: 15,
+		padding: 8,
+		borderTopWidth: 1,
+		borderTopColor: GlobalStyles.colors.primary2,
+		alignItems: 'center',
+	},
+});
