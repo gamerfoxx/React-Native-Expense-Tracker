8c1e1febd82d9e90c5aaca3c5385d0b10fd679b1
gamerfoxx <admin@josephludbrook.com>
2023-08-18 16:17:12 -0400

    format form

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -3,11 +3,17 @@ import { GlobalStyles } from '../../constants/styles';
 
 function Input({ label, textInputConfiguration }) {
 	// textInputConfiguration must be the same values as expected by TextInput
+
+	let inputStyles = [styles.input];
+	if (textInputConfiguration && textInputConfiguration.multiline) {
+		inputStyles.push(styles.inputMultiline);
+	}
+
 	return (
 		<View style={styles.inputContainer}>
 			<Text style={styles.labelText}>{label}</Text>
 			<TextInput
-				style={styles.input}
+				style={inputStyles}
 				{...textInputConfiguration}
 			/>
 		</View>
@@ -19,7 +25,7 @@ export default Input;
 const styles = StyleSheet.create({
 	inputContainer: {
 		marginHorizontal: 5,
-		marginVertical: 15,
+		marginVertical: 6,
 	},
 	labelText: {
 		fontSize: 12,
@@ -32,5 +38,9 @@ const styles = StyleSheet.create({
 		padding: 6,
 		borderRadius: 6,
 		fontSize: 18,
+	},
+	inputMultiline: {
+		minHeight: 100,
+		textAlignVertical: 'top',
 	},
 });
