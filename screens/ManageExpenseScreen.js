48556c7bb9302b471680b47afea3954dc757d911
gamerfoxx <admin@josephludbrook.com>
2023-08-19 12:30:38 -0400

    begin using axios

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -5,6 +5,7 @@ import IconButton from '../components/UI/IconButton';
 import { GlobalStyles } from '../constants/styles';
 import { ExpensesContext } from '../store/expenses-context';
 import ExpenseForm from '../components/ManageExpense/ExpenseForm';
+import { storeExpense } from '../util/http';
 
 function ManageExpenseScreen({ route, navigation }) {
 	const expensesCtx = useContext(ExpensesContext);
@@ -32,6 +33,7 @@ function ManageExpenseScreen({ route, navigation }) {
 		if (isEdited) {
 			expensesCtx.updateExpense(editedExpenseId, expenseData);
 		} else {
+			storeExpense(expenseData);
 			expensesCtx.addExpense(expenseData);
 		}
 		navigation.goBack();
